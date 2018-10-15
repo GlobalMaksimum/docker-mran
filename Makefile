89752f7af219983658badcb7ae38e7a58f5ba4dd
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2018-10-15 16:28:14 +0300

    Create another image including popular datascience libraries for R

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	$(MAKE) -C mran
+	$(MAKE) -C mran-datascience	
---
+++ mran-datascience/test.sh
@@ -0,0 +1,37 @@
+the_world_is_flat=true
+# ...do something interesting...
+if [ "$the_world_is_flat" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
+
+the_world_is_flat=yes
+# ...do something interesting...
+if [ "$the_world_is_flat" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
+
+
+the_world_is_flat=1
+# ...do something interesting...
+if [ "$the_world_is_flat" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
+
+the_world_is_flat=false
+# ...do something interesting...
+if [ "$the_world_is_flat" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
+
+the_world_is_flat=no
+# ...do something interesting...
+if [ "$the_world_is_flat" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
+
+
+the_world_is_flat=0
+# ...do something interesting...
+if [ "$the_world_is_flat" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
